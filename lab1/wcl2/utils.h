00edccb70df8ca8849d0ac756bf9e5f110103f3b
Cannon Lewis <cannon10100@gmail.com>
2017-02-05 13:59:29 -0600

    Fixed utils.h

--- lab1/wcl2/utils.h
+++ lab1/wcl2/utils.h
@@ -1,29 +1,10 @@
-//
-// File containing testing utilities.
-//
+#include <terminals.h>
+#include <hardware.h>
+#include <stdio.h>
 
-#include "utils.h"
+// Print terminal statistics
+void print_stats(struct termstat *stats);
 
-void
-print_stats(struct termstat *stats) {
-    int i;
-    for (i = 0; i < NUM_TERMINALS; i++) {
-        printf("\nTERMINAL %d STATISTICS:\n", i);
-        printf("\tCharacters in (tty_in): %d\n", stats[i].tty_in);
-        printf("\tCharacters out (tty_out): %d\n", stats[i].tty_out);
-        printf("\tUser characters written (user_in): %d\n", stats[i].user_in);
-        printf("\tUser characters read (user_out): %d\n", stats[i].user_out);
-    }
-}
-
-void
-append_null(char *buf, int maxlen) {
-    int j;
-
-    for (j = 0; j < maxlen; j++) {
-        if (buf[j] == '\n') {
-            buf[j + 1] = '\0';
-            break;
-        }
-    }
-}
+// Append a null character to the end of a character array which is assumed to terminate
+// with a newline. Note buf must have size at least maxlen - 1.
+void append_null(char *buf, int maxlen);
